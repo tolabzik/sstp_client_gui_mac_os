7bc6e0a017ec63078610ba3cf2cc008bb8fe3d96
tolabzik <krasnov.anatolii@gmail.com>
2026-09-08 00:38:12 +0300

    Release SSTP Client GUI 1.4.0

--- build.sh
+++ build.sh
@@ -155,9 +155,9 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
   <key>CFBundleIconFile</key>
   <string>AppIcon</string>
   <key>CFBundleVersion</key>
-  <string>9</string>
+  <string>10</string>
   <key>CFBundleShortVersionString</key>
-  <string>1.3.3</string>
+  <string>1.4.0</string>
   <key>LSMinimumSystemVersion</key>
   <string>12.0</string>
   <key>LSApplicationCategoryType</key>
